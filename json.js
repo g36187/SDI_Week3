af41f53fe463978f152889399362f7deba9d0918
Ruben Quizon <quizon@fullsail.edu>
2013-05-23 00:18:38 -0500

    making good progress

---
+++ json.js
@@ -0,0 +1,17 @@
+var fisherman = {
+	"fishermanThree": [
+		{"fisherName": "Roderick",
+		 "fisherRiddle": "What goes in the water black and comes out red?",
+ 		 "fisherAnswer": "A lobster!"
+ 		},
+		{"fisherName": "Beauford",
+		 "fisherRiddle": "What gets wet when drying?",
+ 		 "fisherAnswer": "A towel!"
+  	    },
+		{"fisherName": "Huffington",
+		 "fisherRiddle": "What holds water yet is full of holes?",
+ 		"fisherAnswer": "A Sponge!"
+    	}
+	 ]
+};
+
---
+++ quizon_ruben_project3.js
@@ -0,0 +1,51 @@
+//alert("JavaScript works!");
+
+/* Ruben Quizon
+   SDI 1305
+   Project 3
+   JSONification! 
+*/
+
+// global variables
+var heroFirstName = "Quest",
+	heroLastName = "Questerson",
+	currentLevel = 1,
+	heroIsFamous = False;
+    
+// chores method . 	more info
+var chores = {
+	choreOne: "clean something",
+	choreTwo: boolean value,
+	choreThree: ["a","rr","ay"],
+	deliverTo: function []{
+		stuff goes here
+	}
+};
+
+var choreName = chores. <--make loop //accessor
+chores.deliverTo("pizza"); //mutator
+var getPaid = chores.getMoney(howMuch); //function method
+pirate.repairDamage(damage, ship);  // object argument //fix this later lol
+
+// fishermen three method . more info	
+
+
+
+// use this for reference
+}
+var cube = {
+	"name": "Rubik",
+	"length": 5,
+	"width":  5,
+	"height": 5,
+	"isSquare": true,
+	"area": function(){
+		var totalArea = this.height * this.width * this.length; // don't use this in your own code ..use the getter, though
+		return totalArea;
+	}
+};
+
+console.log("The length of the cube is " + cube.length);
+console.log(cube.area());
+*/
+
