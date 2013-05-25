962f7e49ff51e4e69549503961a9186801dba6bd
Ruben Quizon <quizon@fullsail.edu>
2013-05-24 22:01:15 -0500

    this was much harder than i thought

--- quizon_ruben_project3.js
+++ quizon_ruben_project3.js
@@ -10,14 +10,14 @@
 var heroFirstName = "Quest",
 	heroLastName = "Questerson",
 	currentLevel = 1,
-	heroIsFamous = False;
+	heroIsFamous = "False";
     
 // chores method . 	more info
-var chores = {
-	choreOne: "clean something",
-	choreTwo: boolean value,
-	choreThree: ["a","rr","ay"],
-	deliverTo: function []{
+/*var chores = {
+	"choreOne": "clean something",
+	"choreFinished": false,
+	"placesToDeliver": ["Harry\'s House","Ron\'s Hovel","Bob\'s Bungalow"],
+	"deliverTo": function (){
 		stuff goes here
 	}
 };
@@ -27,12 +27,27 @@ chores.deliverTo("pizza"); //mutator
 var getPaid = chores.getMoney(howMuch); //function method
 pirate.repairDamage(damage, ship);  // object argument //fix this later lol
 
+*/
+
 // fishermen three method . more info	
 
+console.log ("His first taste of adventure came as a complete surpise when his small village");
+console.log ("was attacked by a raiding group of old fishermen!");
+console.log ("\nHe was captured and brought to the Captains Three.  Each had a special riddle");
+console.log ("for him to answer.  If he wasn't able to answer them, he was to be fed to the fishes.\n");
 
+var grabData = function (fisherman){
+	for (var i = 0; i < fisherman.fishermanThree.length; i++) {
+		var name = fisherman.fishermanThree[i];
+		console.log (name.fisherName + "\'s riddle was \"" + name.fisherRiddle + "\"");
+		console.log ("..and our hero answered \"" + name.fisherAnswer + "\"");
+	}
+};
+
+grabData(fisherman);
 
 // use this for reference
-}
+
 var cube = {
 	"name": "Rubik",
 	"length": 5,
@@ -47,5 +62,5 @@ var cube = {
 
 console.log("The length of the cube is " + cube.length);
 console.log(cube.area());
-*/
+
 
